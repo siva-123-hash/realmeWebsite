c3ff047e9ff01b31401a1db698281b50b656f790
Todd Mpeli <toddowenmpeli@rocketmail.com>
2021-10-25 19:40:18 +0100

    Added product color image change when clicked, changes the products color if moss green or rust red

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,4 @@
+/*===== PRODUCT IMAGE CHANGE =====*/
 function changeProductImage1() {
 	// changes the main product image ot the selected image
 	const mainImage = (document.getElementById("mainImage").src =
@@ -5,8 +6,7 @@ function changeProductImage1() {
 
 	// Creates the border style for other additional images
 	const borderLine = document.getElementById("btn1");
-	borderLine.style.borderBottomColor = "#fcd742";
-	borderLine.style.borderBottom = "0.15rem solid";
+	borderLine.style.borderBottom = "#111 0.15rem solid";
 
 	// Removes the border style for other additional images
 	const btn1NoBorderLine = document.getElementById("btn2");
@@ -24,8 +24,7 @@ function changeProductImage2() {
 
 	// Creates the border style for other additional images
 	const borderLine = document.getElementById("btn2");
-	borderLine.style.borderBottomColor = "#fcd742";
-	borderLine.style.borderBottom = "0.15rem solid";
+	borderLine.style.borderBottom = "#111 0.15rem solid";
 
 	// Removes the border style for other additional images
 	const btn1NoBorderLine = document.getElementById("btn1");
@@ -43,8 +42,7 @@ function changeProductImage3() {
 
 	// Creates the border style for other additional images
 	const borderLine = document.getElementById("btn3");
-	borderLine.style.borderBottomColor = "#fcd742";
-	borderLine.style.borderBottom = "0.15rem solid";
+	borderLine.style.borderBottom = "#111 0.15rem solid";
 
 	// Removes the border style for other additional images
 	const btn1NoBorderLine = document.getElementById("btn1");
@@ -62,8 +60,7 @@ function changeProductImage4() {
 
 	// Creates the border style for other additional images
 	const borderLine = document.getElementById("btn4");
-	borderLine.style.borderBottomColor = "#fcd742";
-	borderLine.style.borderBottom = "0.15rem solid";
+	borderLine.style.borderBottom = "#111 0.15rem solid";
 
 	// Removes the border style for other additional images
 	const btn1NoBorderLine = document.getElementById("btn1");
@@ -72,4 +69,52 @@ function changeProductImage4() {
 	btn1NoBorderLine.style.borderBottom = "none";
 	btn2NoBorderLine.style.borderBottom = "none";
 	btn3NoBorderLine.style.borderBottom = "none";
+}
+
+/*===== CHANGE PRODUCT COLOUR IMAGE =====*/
+// Moss Green realme X50 Pro 5G product images
+function mossGreenImages() {
+	// Main product images
+	document.getElementById("mainImage").src =
+		"/src/img/productImages/1594595165858.jpg";
+
+	// More products images
+	document.getElementById("moreImage1").src =
+		"/src/img/productImages/1594595165858.jpg";
+
+	document.getElementById("moreImage2").src =
+		"/src/img/productImages/1588460890879.jpg";
+
+	document.getElementById("moreImage3").src =
+		"/src/img/productImages/1588460904321.jpg";
+
+	document.getElementById("moreImage4").src =
+		"/src/img/productImages/1588460897805.jpg";
+
+	// Button styles changes
+	document.getElementById("mossGreen").style.border = "#111 0.1rem solid";
+	document.getElementById("rustRed").style.border = "#d3d3d3 0.1rem solid";
+}
+// Rust Red realme X50 Pro 5G products images
+function rushRedImages() {
+	// Main product images
+	document.getElementById("mainImage").src =
+		"/src/img/productImages/1594595141366.jpg";
+
+	// More products images
+	document.getElementById("moreImage1").src =
+		"/src/img/productImages/1594595141366.jpg";
+
+	document.getElementById("moreImage2").src =
+		"/src/img/productImages/1588462321862.jpg";
+
+	document.getElementById("moreImage3").src =
+		"/src/img/productImages/1588462321864.jpg";
+
+	document.getElementById("moreImage4").src =
+		"/src/img/productImages/1588462321863.jpg";
+
+	// Button styles changes
+	document.getElementById("rustRed").style.border = "#111 0.1rem solid";
+	document.getElementById("mossGreen").style.border = "#d3d3d3 0.1rem solid";
 }
