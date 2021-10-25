7850451707054bc6f2effb9ce8e86a9198b4113a
Todd Mpeli <toddowenmpeli@rocketmail.com>
2021-10-25 18:47:27 +0100

    Create a javascript functionto animate the different images when clicked for the products page. Also updated some of the stying for the same page. Lastly created new button functions when clicked

---
+++ src/js/app.js
@@ -0,0 +1,67 @@
+function changeProductImage1() {
+	const mainImage = (document.getElementById("mainImage").src =
+		"/src/img/productImages/1594595141366.jpg");
+
+	const borderLine = document.getElementById("btn1");
+
+	borderLine.style.borderBottomColor = "#111";
+	borderLine.style.borderBottom = "0.15rem solid";
+
+	const btn1NoBorderLine = document.getElementById("btn2");
+	const btn2NoBorderLine = document.getElementById("btn3");
+	const btn3NoBorderLine = document.getElementById("btn4");
+	btn1NoBorderLine.style.borderBottom = "none";
+	btn2NoBorderLine.style.borderBottom = "none";
+	btn3NoBorderLine.style.borderBottom = "none";
+}
+
+function changeProductImage2() {
+	const mainImage = (document.getElementById("mainImage").src =
+		"/src/img/productImages/1588462321862.jpg");
+
+	const borderLine = document.getElementById("btn2");
+
+	borderLine.style.borderBottomColor = "#111";
+	borderLine.style.borderBottom = "0.15rem solid";
+
+	const btn1NoBorderLine = document.getElementById("btn1");
+	const btn2NoBorderLine = document.getElementById("btn3");
+	const btn3NoBorderLine = document.getElementById("btn4");
+	btn1NoBorderLine.style.borderBottom = "none";
+	btn2NoBorderLine.style.borderBottom = "none";
+	btn3NoBorderLine.style.borderBottom = "none";
+}
+
+function changeProductImage3() {
+	const mainImage = (document.getElementById("mainImage").src =
+		"/src/img/productImages/1588462321864.jpg");
+
+	const borderLine = document.getElementById("btn3");
+
+	borderLine.style.borderBottomColor = "#111";
+	borderLine.style.borderBottom = "0.15rem solid";
+
+	const btn1NoBorderLine = document.getElementById("btn1");
+	const btn2NoBorderLine = document.getElementById("btn2");
+	const btn3NoBorderLine = document.getElementById("btn4");
+	btn1NoBorderLine.style.borderBottom = "none";
+	btn2NoBorderLine.style.borderBottom = "none";
+	btn3NoBorderLine.style.borderBottom = "none";
+}
+
+function changeProductImage4() {
+	const mainImage = (document.getElementById("mainImage").src =
+		"/src/img/productImages/1588462321863.jpg");
+
+	const borderLine = document.getElementById("btn4");
+
+	borderLine.style.borderBottomColor = "#111";
+	borderLine.style.borderBottom = "0.15rem solid";
+
+	const btn1NoBorderLine = document.getElementById("btn1");
+	const btn2NoBorderLine = document.getElementById("btn2");
+	const btn3NoBorderLine = document.getElementById("btn3");
+	btn1NoBorderLine.style.borderBottom = "none";
+	btn2NoBorderLine.style.borderBottom = "none";
+	btn3NoBorderLine.style.borderBottom = "none";
+}
